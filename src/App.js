1d55b052bae8acb602ccc32b7c1c0dea045514a6
Duncan Moyo <duncanfmoyo@gmail.com>
2020-11-05 20:08:10 +0200

    replacing redux with a useReducer hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,56 @@
-import React, {useContext} from 'react'
+import React, { useContext, useReducer } from "react";
 import { UserContext } from "./index";
 
-const App = () => {
+const initialState = {
+  count: 0,
+};
 
-  const value = useContext(UserContext)
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return {
+        count: state.count + 1,
+      };
+    case "decrement":
+      return {
+        count: state.count - 1,
+      };
+    case 'reset':
+      return {
+        initialState
+      }
+    default:
+      return initialState;
+  }
+};
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const value = useContext(UserContext);
 
   return (
     <div>
-      Hello, {value}.
+      Count: {state.count}
+      <button
+        className="border p-1 m-1"
+        onClick={() => dispatch({ type: "increment" })}
+      >
+        Increment
+      </button>
+
+      <button
+        className="border p-1 m-1"
+        onClick={() => dispatch({ type: "decrement" })}
+      >
+        Decrement
+      </button>
+      <button
+        className="border p-1 m-1"
+        onClick={() => dispatch({ type: "reset" })}
+      >
+        Reset
+      </button>
     </div>
   );
 };
